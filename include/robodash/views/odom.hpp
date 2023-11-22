5a557aefa656c9c5d4ce416696be412cb566ca54
SteveTheSteve1400 <you@example.com>
2023-11-21 21:12:01 -0800

    first commit

---
+++ include/robodash/views/odom.hpp
@@ -0,0 +1,14 @@
+#include "robodash/api.h"
+#include <string>
+
+
+namespace rd{
+    class OdomView{
+        private: 
+            rd_view_t *view;
+
+        public:
+
+        
+    };
+}
---
+++ src/uiLIB/fileManager.cpp
@@ -0,0 +1,67 @@
+#include "main.h"
+
+lv_fs_res_t pcfs_open( void * file_p, const char * fn, lv_fs_mode_t mode){
+    errno = 0;
+    const char * flags = "";
+    if(mode == LV_FS_MODE_WR) flags = "wb";
+    else if(mode == LV_FS_MODE_RD) flags = "rb";
+    else if(mode == (LV_FS_MODE_WR | LV_FS_MODE_RD)) flags = "a+";
+
+    char buf[256];
+    sprintf(buf, "/%s", fn);
+    pc_file_t f = fopen(buf, flags);
+
+    if(f == NULL)
+      return LV_FS_RES_UNKNOWN;
+    else {
+      fseek(f, 0, SEEK_SET);
+      pc_file_t * fp = (pc_file_t *)file_p;
+      *fp = f;
+    }
+
+    return LV_FS_RES_OK;
+}
+
+lv_fs_res_t pcfs_close( void * file_p)
+{
+    pc_file_t * fp = (pc_file_t *)file_p;
+    fclose(*fp);
+    return LV_FS_RES_OK;
+}
+
+lv_fs_res_t pcfs_read( void * file_p, void * buf, uint32_t btr, uint32_t * br)
+{
+    pc_file_t * fp =  (pc_file_t *)file_p;
+    *br = fread(buf, 1, btr, *fp);
+    return LV_FS_RES_OK;
+}
+
+lv_fs_res_t pcfs_seek( void * file_p, uint32_t pos)
+{
+    pc_file_t * fp = (pc_file_t *)file_p;
+    fseek(*fp, pos, SEEK_SET);
+    return LV_FS_RES_OK;
+}
+
+lv_fs_res_t pcfs_tell( void * file_p, uint32_t * pos_p)
+{
+    pc_file_t * fp =  (pc_file_t *)file_p;
+    *pos_p = ftell(*fp);
+    return LV_FS_RES_OK;
+}
+
+
+void pcfs_init(){
+    lv_fs_drv_t pcfs_drv;
+    lv_fs_drv_init(&pcfs_drv);
+
+    pcfs_drv.file_size = sizeof(pc_file_t);
+    pcfs_drv.letter = 'P';
+    pcfs_drv.open_cb = pcfs_open;
+    pcfs_drv.close_cb = pcfs_close;
+    pcfs_drv.read_cb = pcfs_read;
+    pcfs_drv.seek_cb = pcfs_seek;
+    pcfs_drv.tell_cb = pcfs_tell;
+
+    lv_fs_drv_register(&pcfs_drv);
+}
